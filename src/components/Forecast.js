5dbf53d3d729bcacdf3f144cebfbfbbe20f24815
Ollie Lee <myself.ollie.lee@gmail.com>
2020-04-29 22:27:08 +0930

    icon failed

---
+++ src/components/Forecast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+    const api = `https://www.metaweather.com/api/location/search/?query=adelaide`;
+export default class Forecast extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+
+        
+    }
+
+    render() {
+        return (<>
+
+        </>);
+    }
+}
